83fb15c36b93381d04793e5279d2a00a915be10e
Sam Nguyen <samxnguyen@gmail.com>
2012-04-18 11:42:21 -0700

    update 41

--- 41.hs
+++ 41.hs
@@ -5,4 +5,4 @@ isPandigital :: Int -> Bool
 isPandigital n = (sort $ digits n) == [1..len]
     where len = length $ show n
 
-pandigitals = filter isPandigital [0..]
+pandigitals = filter isPandigital [0..987654321]
